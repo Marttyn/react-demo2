76d73f60aa7fe7d16c39a2609564362c0d09b71c
Rafael Oliveira <44215275+Marttyn@users.noreply.github.com>
2023-09-25 19:46:42 +0000

    -Added custom components for Date, Item, Expenses

---
+++ src/components/Expenses.js
@@ -0,0 +1,14 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses({ expenses }) {
+  return (
+    <div className="expenses">
+      <ExpenseItem {...expenses[0]} />
+      <ExpenseItem {...expenses[1]} />
+      <ExpenseItem {...expenses[2]} />
+    </div>
+  );
+}
+
+export default Expenses;
